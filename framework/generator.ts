eb9a06d01b7b293b34b991bc7d20f097a4fa6665
Titor-Z <titor@h2ostudio.cn>
2021-12-29 07:47:46 +0000

    ✨ ADD  GeneratorPath

--- framework/generator.ts
+++ framework/generator.ts
@@ -1,3 +1,4 @@
+import { resolve } from "path/posix"
 import { cwd } from "process"
 
 export default class Generator {
@@ -8,7 +9,11 @@ export default class Generator {
   protected templateResolvePath: string = '/templates/'
 
   constructor () {
-    this.workspace = cwd()    
+    // 当前脚手架路径分隔符
+    const GeneratorPathSeparator = 'dist'
+
+    this.workspace = cwd()
+    this.generatorPath = resolve(__dirname).substring(0, resolve(__dirname).indexOf(GeneratorPathSeparator) )
   }
 
 }
